b426f174d9cf5ca0fcee2e9009a685a3d7b28e17
Oleksii Borysenko <343502597s@gmail.com>
2023-11-25 21:05:26 +0100

    part1

---
+++ src/main/java/mate/academy/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,61 @@
+package mate.academy.service.impl;
+
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Inject
+    private TicketDao ticketDao;
+    private ShoppingCartDao shoppingCartDao;
+
+
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+       // shoppingCartDao.update();
+       // ticketDao.add()
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        return shoppingCartDao.getByUser(user).get();
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUser(user);
+        shoppingCartDao.add(shoppingCart);
+    }
+
+    @Override
+    public void clear(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.detach(shoppingCart);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can`t clear in DB Shopping cart= " + shoppingCart,e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
